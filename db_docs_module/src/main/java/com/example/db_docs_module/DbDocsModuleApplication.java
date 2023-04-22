133ec969fd6c91262037f34bcfa028ceb78b2e3b
unknown <kossart2001@gmail.com>
2023-04-22 21:44:32 +0300

    db_docs_module initial commit

---
+++ db_docs_module/src/main/java/com/example/db_docs_module/DbDocsModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.db_docs_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbDocsModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbDocsModuleApplication.class, args);
+    }
+
+}
