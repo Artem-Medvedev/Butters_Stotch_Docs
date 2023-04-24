db85b7dab7d387eb66c716d288b9508e42ee07bf
unknown <kossart2001@gmail.com>
2023-04-24 17:46:03 +0300

    saving and reading pdf file from database

--- db_docs_module/src/main/java/com/example/db_docs_module/consoleTest/TestDB.java
+++ db_docs_module/src/main/java/com/example/db_docs_module/consoleTest/TestDB.java
@@ -6,9 +6,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -23,10 +26,20 @@ public class TestDB implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Path path = Paths.get("C:\\Users\\Artem\\IdeaProjects\\Butters_Stotch_Docs\\db_docs_module\\resume_version2.0.pdf");
-        byte[] pdfData = Files.readAllBytes(path);
+//        Path path = Paths.get("C:\\Users\\Artem\\IdeaProjects\\Butters_Stotch_Docs\\db_docs_module\\resume_version2.0.pdf");
+//        byte[] pdfData = Files.readAllBytes(path);
+//
+//        Document doc = new Document(1l,"bruh", ".pdf", pdfData);
+//        documentRepository.save(doc);
 
-        Document doc = new Document(1l,"bruh", ".pdf", pdfData);
-        documentRepository.save(doc);
+        Document document = documentRepository.findAll().get(0);
+
+
+        try (FileOutputStream fos = new FileOutputStream(document.getDocumentName() + document.getDocumentExtension())) {
+            fos.write(document.getDocument()); // Write the byte array to the file
+            System.out.println("PDF file saved successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- db_docs_module/src/main/java/com/example/db_docs_module/enity/Document.java
+++ db_docs_module/src/main/java/com/example/db_docs_module/enity/Document.java
@@ -1,7 +1,9 @@
 package com.example.db_docs_module.enity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,12 +11,18 @@ import lombok.NoArgsConstructor;
 @Data
 @Entity
 @AllArgsConstructor
+@Table(name = "documents")
 @NoArgsConstructor(force = true)
 public class Document {
 
     @Id
+    @Column(name = "document_id")
     private final long id;
+    @Column(name = "document_name")
     private final String documentName;
+
+    @Column(name = "document_extension")
     private final String documentExtension;
+    @Column(name = "document_data")
     private final byte[] document;
 }
