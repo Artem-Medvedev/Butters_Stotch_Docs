7d32522dfeaec1c5654f4a50c809efb8cf5a6b00
unknown <kossart2001@gmail.com>
2023-04-24 17:32:12 +0300

    test db work

--- db_docs_module/src/main/java/com/example/db_docs_module/consoleTest/TestDB.java
+++ db_docs_module/src/main/java/com/example/db_docs_module/consoleTest/TestDB.java
@@ -1,8 +1,17 @@
 package com.example.db_docs_module.consoleTest;
 
+import com.example.db_docs_module.enity.Document;
 import com.example.db_docs_module.repository.DocumentRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
+@Slf4j
 public class TestDB implements CommandLineRunner {
 
     private final DocumentRepository documentRepository;
@@ -14,6 +23,10 @@ public class TestDB implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        
+        Path path = Paths.get("C:\\Users\\Artem\\IdeaProjects\\Butters_Stotch_Docs\\db_docs_module\\resume_version2.0.pdf");
+        byte[] pdfData = Files.readAllBytes(path);
+
+        Document doc = new Document(1l,"bruh", ".pdf", pdfData);
+        documentRepository.save(doc);
     }
 }
--- db_docs_module/src/main/java/com/example/db_docs_module/repository/DocumentRepository.java
+++ db_docs_module/src/main/java/com/example/db_docs_module/repository/DocumentRepository.java
@@ -3,5 +3,5 @@ package com.example.db_docs_module.repository;
 import com.example.db_docs_module.enity.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DocumentRepository extends JpaRepository<Long, Document> {
+public interface DocumentRepository extends JpaRepository<Document, Long> {
 }
